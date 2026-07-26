bafce3b2d14e603833681687fd43fe08e430e9ca
bugblazer <daregamer2004@gmail.com>
2026-07-27 02:32:22 +0500

    Added CORS support for frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,4 @@
+const cors = require("cors");
 const express = require("express");
 
 const app = express();
@@ -6,6 +7,7 @@ const indexRoutes = require("./routes/indexRoutes");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/", indexRoutes);
